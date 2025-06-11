53ec770c206c1bdd232dbac3f17194bce11bf830
tharriman <harrim58@email.franklin.edu>
2025-06-11 10:21:35 -0500

    Update page.jsx

--- sp25_echo/app/registered_user/about-us/page.jsx
+++ sp25_echo/app/registered_user/about-us/page.jsx
@@ -7,7 +7,14 @@ export default function AboutUs() {
                 We're not your average bakery. <span className="text-black">Rolls by the Pound </span> is dedicated to crafting exceptional rolls using only the finest ingredients.
                 Baked fresh daily, our rolls are sold by weight, ensuring you get exactly the amount you need. Taste the difference quality makes, one pound at a time.
                 </h2>
-            </div> 
+            </div>
+            <div className="mt-10 w-full md:w-9/12 sm:pb-[50px]">
+                <iframe
+                    className="w-full rounded-lg"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3053.709039309633!2d-82.45266842379927!3d40.059593671499364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88381672df4fffff%3A0x6dbc711b3b52be65!2sRolls%20By%20The%20Pound%20-%20Coffee%20and%20Treats!5e0!3m2!1sen!2sus!4v1742150973590!5m2!1sen!2sus" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"
+                    height={450}
+                ></iframe>
+            </div>
         </main>
     );
 }
